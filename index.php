36ead85cb9c38295ae4229c99b6ea1b51032d301
Keshav Sharma <13808150+Keshav2136@users.noreply.github.com>
2022-08-20 17:48:10 +0530

    .

--- index.php
+++ index.php
@@ -6,8 +6,14 @@ if (x==12&y==15) {
     x++;
     y++;
 }
+elseif (x==13) {
+    
+}
+else {
+    echo "no";
+}
 
-for ($i=0; $i < 12; $i++) { 
+for ($i=0; $i < 5; $i++) { 
     echo "<h1>Good</h1>";
 }
 ?>
