ab3673ab78ddedd32283159ccb3e9dd9fa514a88
keshav2136 <keshav.simple.123@gmail.com>
2022-09-04 14:49:44 +0530

    Rand func

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
 
-echo 0;
-
-$s=array("hi"=>"yo","hnh");
-print_r($s);
-print_r(array_count_values($s));
-echo $s;
-
-function run(){
-    print_r("Nice");
-}
-run();
-
 $city=array("Delhi","Mumbai","Kolkata","Hyderabad");
 $state=array("Haryana", "Punjab","Assam","Tripura");
 
-for($i=0;$i<=4;$i++){
-    echo (rand($city[0],$city[3]))."";
-}
-print_r (rand($city[0],$city[3]));
-print_r (rand($state[0],$state[3]));
+$k=rand(0,count($city));
+$m=rand(0,count($state));
+echo $city[$k]." ". $state[$m];
 
 ?>
