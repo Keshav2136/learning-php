5e1ed76bff50bdb4ec9a4a6e2f2d7cb5eed30cea
keshav2136 <keshav.simple.123@gmail.com>
2022-09-06 20:03:20 +0530

    add more

--- index.php
+++ index.php
@@ -3,11 +3,14 @@
 #Declaring Variables
 $a=12; $b=14;
 
+#Data Types- string, integer, boolen, array, double
+
 #Datatype- Arrays
 $k=array("h1","h1","h1");
+$j=true;
 
 #Getting datatype (using predefined functions of PHP)
-echo gettype($k);
+echo gettype($k).gettype($j);
 
 #Increment / Decrement Operators
 $g=$a++;
@@ -15,11 +18,23 @@ echo $g++;
 echo $g;
 
 #Conditional statements
-## Using if-else
+ ## Using if-else
 if ($a=12 && $b=14){
     echo "true";
 }else{
     echo "fa";
+}
+
+ #Nested loop
+if($a==12 && $b==14){
+    echo "Good";
+}elseif ($a==122) {
+    echo "";
+}elseif($b==122){
+    echo "";
+}
+else{
+    echo "";
 }
 
 $city=array("Delhi","Mumbai","Kolkata","Hyderabad");
@@ -29,5 +44,16 @@ $state=array("Haryana", "Punjab","Assam","Tripura");
 $k=rand(0,count($city));
 $m=rand(0,count($state));
 echo $city[$k]." ". $state[$m];
+
+
+
+#Classes
+
+class Hello{
+    public $hello;
+    public function hello(){
+        echo "This is a method in class";
+    }
+}
 
 ?>
