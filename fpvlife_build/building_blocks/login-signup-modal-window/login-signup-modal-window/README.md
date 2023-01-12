53f2abf31920a8a75157dc29cee9cef459f8ac27
Keshav Sharma <13808150+Keshav2136@users.noreply.github.com>
2023-01-13 01:51:58 +0530

    Add all files created till now for practicing PHP

---
+++ fpvlife_build/building_blocks/login-signup-modal-window/login-signup-modal-window/README.md
@@ -0,0 +1,5 @@
+# Login/Signup Modal Window
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/adventuresinmissions/pen/poWpWw](https://codepen.io/adventuresinmissions/pen/poWpWw).
+
+This modal window allows users to login/signup into your website. Once opened, the user can easily switch from one form to the other, or select the reset password option. http://codyhouse.co/gem/loginsignup-modal-window/
---
+++ fpvlife_build/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+	<link rel="stylesheet" href="core_components/top_header.css">
+</head>
+<body>
+
+<section id="top_header">
+    <!--This contains the ham menu, login/signup menu, logo, branding-->
+</section>
+
+<?php include ('core_components/top_header.php');?>
+
+
+<section id="upper_middle_section">
+    <!--Here goes the introduction of our brand-->
+</section>
+
+
+<!--
+<form action="welcome.php" method="post">
+First Name: <input type="text" name="firstname"><br>
+Last Name: <input type="text" name="lastname"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="text" name="password"><br>
+State: <input type="text" name="state"><br>
+<input type="submit" name="submit"/>
+</form>
+-->
+
+	<script src="static/jquery.min.js"></script>
+	<script src="core_components/top_header.js"></script>
+</body>
+</html>
